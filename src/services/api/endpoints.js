37f3f3b934f0c1dd3762c2042cd98908857d5460
ismay <ismay@ismaywolff.nl>
2017-01-04 16:13:28 +0100

    Update env variable

--- src/services/api/endpoints.js
+++ src/services/api/endpoints.js
@@ -2,7 +2,7 @@ let base
 
 // istanbul ignore if: this will never be reached when testing
 if (process.env.NODE_ENV === 'production') {
-  base = `${process.env.PROD_BASE}/api/v2`
+  base = `${process.env.PROD_API}/api/v2`
 } else {
   base = `${process.env.DEV_API}/api/v2`
 }
