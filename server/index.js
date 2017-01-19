4ffb87a099744cd67f890978479a23d46414c56b
ismay <ismay@ismaywolff.nl>
2017-01-19 10:59:53 +0100

    Separate client and server code

--- server/index.js
+++ server/index.js
@@ -2,18 +2,18 @@ const express = require('express')
 
 const app = express()
 
-// Redirect the root path to the login screen.
+// redirect the root path to the login screen
 app.get('/', (req, res) => {
   res.redirect('/login')
 })
 
-// Serve the index.html on the login route.
+// serve the index.html on the login route
 app.get('/login', (req, res) => {
   res.sendFile('/index.html')
 })
 
-// Serve all static file in the project folder.
+// serve all static file in the project folder
 app.use(express.static(__dirname))
 
-// Start the server on port 80.
+// start the server on port 80
 app.listen(80)
