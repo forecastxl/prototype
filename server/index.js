ad9493375f3d726b030be4acff0ddec5ec1f9da5
ismay <ismay@ismaywolff.nl>
2017-01-25 10:26:44 +0100

    Update server config

--- server/index.js
+++ server/index.js
@@ -2,18 +2,13 @@ const express = require('express')
 
 const app = express()
 
-// redirect the root path to the login screen
-app.get('/', (req, res) => {
-  res.redirect('/login')
-})
+// serve all static files in the project folder
+app.use(express.static(__dirname))
 
 // serve the index.html on the login route
-app.get('/login', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile('/index.html')
 })
-
-// serve all static file in the project folder
-app.use(express.static(__dirname))
 
 // start the server on port 80
 app.listen(80)
