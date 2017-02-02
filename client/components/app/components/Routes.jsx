2a30083d6caaf904e4bfd69d9384628a61239956
ismay <ismay@ismaywolff.nl>
2017-02-02 17:04:46 +0100

    Add style

--- client/components/app/components/Routes.jsx
+++ client/components/app/components/Routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Match from 'react-router/Match'
+import styled from 'styled-components'
 import { Header } from '../../header'
 import { Home } from '../../home'
 import {
@@ -12,18 +13,27 @@ import {
   SignupSuccess
 } from '../../user'
 
+const Container = styled.div`
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 function Routes() {
   return (
     <div>
       <Header />
-      <Match exactly pattern="/" component={Home} />
-      <Match exactly pattern="/login" component={LoginFormContainer} />
-      <Match exactly pattern="/login/forgot-password" component={ForgotPasswordContainer} />
-      <Match pattern="/login/forgot-password/success" component={ForgotPasswordSuccess} />
-      <Match pattern="/login/reset-password" component={ResetPasswordContainer} />
-      <Match exactly pattern="/signup" component={SignupFormContainer} />
-      <Match pattern="/signup/confirm" component={SignupConfirmContainer} />
-      <Match pattern="/signup/success" component={SignupSuccess} />
+      <Container>
+        <Match exactly pattern="/" component={Home} />
+        <Match exactly pattern="/login" component={LoginFormContainer} />
+        <Match exactly pattern="/login/forgot-password" component={ForgotPasswordContainer} />
+        <Match pattern="/login/forgot-password/success" component={ForgotPasswordSuccess} />
+        <Match pattern="/login/reset-password" component={ResetPasswordContainer} />
+        <Match exactly pattern="/signup" component={SignupFormContainer} />
+        <Match pattern="/signup/confirm" component={SignupConfirmContainer} />
+        <Match pattern="/signup/success" component={SignupSuccess} />
+      </Container>
     </div>
   )
 }
--- client/components/header/components/Header.jsx
+++ client/components/header/components/Header.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router'
+
+const StyledHeader = styled.header`
+  background: #eaf3ef;
+  padding: 0 1rem;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+`
+
+const StyledLink = styled(Link)`
+margin-left: 0.5rem;
+margin-right: 0.5rem;
+  &:first-of-type {
+    margin-left: 0;
+  }
+  &:last-of-type {
+    margin-right: 0;
+  }
+`
 
 function Header() {
   return (
-    <header>
+    <StyledHeader>
       <h1>Forecast XL</h1>
-    </header>
+      <nav>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/login">Login</StyledLink>
+        <StyledLink to="/signup">Signup</StyledLink>
+      </nav>
+    </StyledHeader>
   )
 }
 
