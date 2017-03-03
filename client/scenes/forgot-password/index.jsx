c837faea5f8d9b3a687f344e5fb522e77bdc44e5
ismay <ismay@ismaywolff.nl>
2017-03-03 13:45:17 +0100

    Move to scenes

---
+++ client/scenes/forgot-password/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ForgotPasswordContainer } from '../../components/user'
+
+function Scene() {
+  return (
+    <ForgotPasswordContainer />
+  )
+}
+
+export default Scene
---
+++ client/scenes/signup/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SignupFormContainer } from '../../components/user'
+
+function Scene() {
+  return (
+    <SignupFormContainer />
+  )
+}
+
+export default Scene
