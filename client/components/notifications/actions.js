1e54ed519ca72d1f46cd541df586bfd4de9550f4
ismay <ismay@ismaywolff.nl>
2017-04-14 12:52:47 +0200

    Add hide notification action

--- client/components/notifications/actions.js
+++ client/components/notifications/actions.js
@@ -1,3 +1,4 @@
 import Notifications from 'react-notification-system-redux'
 
 export const addNotification = ({ level, options }) => Notifications[level](options)
+export const hideNotification = uid => Notifications.hide(uid)
--- client/components/notifications/actions.test.js
+++ client/components/notifications/actions.test.js
@@ -6,4 +6,8 @@ describe('actioncreators', () => {
     const options = { title: 'title', message: 'message', uid: 1 }
     expect(actions.addNotification({ level, options })).toMatchSnapshot()
   })
+
+  it('should create a RNS_HIDE_NOTIFICATION action', () => {
+    expect(actions.hideNotification(1)).toMatchSnapshot()
+  })
 })
