684972b2f226216828d050ff42a1f084156f9e66
ismay <ismay@ismaywolff.nl>
2017-05-03 16:59:30 +0200

    Fix nested routes

--- src/server/index.js
+++ src/server/index.js
@@ -7,13 +7,18 @@ const server = express()
 // don't identify as an express server
 server.disable('x-powered-by')
 
-// serve all static files from the /public folder
+// serve all static files from the public folder
 server.use(
   express.static(path.join(__dirname, 'public'), {
     maxAge: '1y',
     setHeaders
   })
 )
+
+// serve index.html for all routes
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 // listen on port 80
 server.listen(80)
