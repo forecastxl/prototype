141fc05cc8ef0348eaeaef13882b0f2663e62654
ismay <ismay@ismaywolff.nl>
2017-05-10 14:30:17 +0200

    Refactor selectors

--- src/client/components/notifications/selectors.test.js
+++ src/client/components/notifications/selectors.test.js
@@ -1,9 +1,9 @@
 import * as selectors from './selectors'
 
-describe('getNotifications', () => {
+describe('getNotificationState', () => {
   it('should return notifications state', () => {
     const state = { notifications: ['message'] }
-    const actual = selectors.getNotifications(state)
+    const actual = selectors.getNotificationState(state)
     const expected = state.notifications
 
     expect(actual).toEqual(expected)
