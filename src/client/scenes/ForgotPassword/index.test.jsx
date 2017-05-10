73c8af8d2fe232926ef5b5d7fc7c4e9300feabe3
ismay <ismay@ismaywolff.nl>
2017-05-10 15:10:32 +0200

    Update scene naming

--- src/client/scenes/ForgotPassword/index.test.jsx
+++ src/client/scenes/ForgotPassword/index.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
-import Scene from './index'
+import ForgotPassword from './index'
 
-describe('<Scene />', () => {
+describe('<ForgotPassword />', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(<Scene />)
+    const wrapper = shallow(<ForgotPassword />)
     expect(shallowToJson(wrapper)).toMatchSnapshot()
   })
 })
