c414981b267d755724833800dfe7129f181a9ac9
ismay <ismay@ismaywolff.nl>
2017-05-12 17:31:07 +0200

    Update signup

--- src/client/components/signup/actions.js
+++ src/client/components/signup/actions.js
@@ -3,7 +3,9 @@ import * as types from './actionTypes'
 
 export const createAccount = createAction(types.CREATE_ACCOUNT)
 export const createAccountSuccess = createAction(types.CREATE_ACCOUNT_SUCCESS)
-export const createAccountFailure = createAction(types.CREATE_ACCOUNT_FAILURE)
+export const createAccountValidationFailure = createAction(types.CREATE_ACCOUNT_VALIDATION_FAILURE)
+export const createAccountClientFailure = createAction(types.CREATE_ACCOUNT_CLIENT_FAILURE)
+export const createAccountServerFailure = createAction(types.CREATE_ACCOUNT_SERVER_FAILURE)
 
 export const confirmAccount = createAction(types.CONFIRM_ACCOUNT)
 export const confirmAccountSuccess = createAction(types.CONFIRM_ACCOUNT_SUCCESS)
