04bdcbc3067290a4ff69d8dcc5345fdcb6c0eab3
ismay <ismay@ismaywolff.nl>
2017-05-12 12:39:02 +0200

    Name tests after the component being tested

--- src/client/components/password/components/ForgotPasswordForm.test.jsx
+++ src/client/components/password/components/ForgotPasswordForm.test.jsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
 import { DumbForgotPasswordForm } from './ForgotPasswordForm'
 
-describe('<ForgotPasswordForm />', () => {
+describe('<DumbForgotPasswordForm />', () => {
   it('renders correctly', () => {
     const wrapper = shallow(
       <DumbForgotPasswordForm onSubmit={() => {}} onChange={() => {}} email={''} errors={{}} />
