e32ccd243f5c54136bc45b87b3029f7b2385832e
ismay <ismay@ismaywolff.nl>
2017-05-19 13:49:58 +0200

    Fix scene naming

--- src/client/scenes/Profile/index.test.jsx
+++ src/client/scenes/Profile/index.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
-import Scene from './index'
+import Profile from './index'
 
-describe('<Scene />', () => {
+describe('<Profile />', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(<Scene />)
+    const wrapper = shallow(<Profile />)
     expect(shallowToJson(wrapper)).toMatchSnapshot()
   })
 })
