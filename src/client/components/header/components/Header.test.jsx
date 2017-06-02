ea37a7d406d3f646b8dad345e30fe5d0bc5d73e5
mkrause <maikelkrause@gmail.com>
2017-06-02 21:41:41 +0200

    (#221) Add test for login button.

--- src/client/components/header/components/Header.test.jsx
+++ src/client/components/header/components/Header.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
+import { RaisedButton } from 'material-ui'
 import { DumbHeader } from './Header'
 
 describe('<DumbHeader />', () => {
@@ -12,5 +13,12 @@ describe('<DumbHeader />', () => {
   it('renders with a profile widget when we are authenticated', () => {
     const wrapper = shallow(<DumbHeader hasAuth history={{}} />)
     expect(shallowToJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('navigates to the login page when clicking the login button', () => {
+    const push = jest.fn()
+    const wrapper = shallow(<DumbHeader hasAuth={false} history={{ push }} />)
+    wrapper.find(RaisedButton).simulate('click')
+    expect(push).toBeCalledWith('/login')
   })
 })
